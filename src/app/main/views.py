0360097c5e7a226d4c9d872953e6f69c14235073
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-06 18:45:21 -0500

    build main viewfunction

---
+++ src/app/main/views.py
@@ -0,0 +1,37 @@
+import sys
+from datetime import datetime
+from flask import (
+    render_template,
+    flash,
+    redirect,
+    url_for,
+    request,
+    current_app
+)
+from flask_login import current_user, login_required
+from app.extensions import db
+from app.catalog import catalog
+from app.models import (
+    CatalogItem,
+    Category,
+)
+
+
+@main.route('/')
+@main.route('/index')
+def index(category):
+    if category is null:
+        catalog_items=CatalogItem.query \
+            .order_by(id)
+        current_category = 'All Items';
+    else:
+        catalog_items = CatalogItem.query \
+                .filter_by(category=category) \
+                .order_by(name)
+        current_category = Category.query \
+                .filter_by(category=category) \
+                .first_or_default().name
+
+    return render_template('catalog/index.html,'
+                            catalog_items=catalog_items,
+                            current_category=current_category)
