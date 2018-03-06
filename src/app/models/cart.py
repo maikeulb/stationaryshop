fa21eaff188ffec55e384748a36de2c4ec95f0c4
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-06 03:27:31 -0500

    add cart domain model

---
+++ src/app/models/cart.py
@@ -0,0 +1,19 @@
+from app.extensions import db
+from datetime import datetime
+from time import time
+from flask import current_app
+from app.search import add_to_index, remove_from_index, query_index
+from app.models.message import Message
+
+
+class Cart.Model):
+    __tablename__ = 'carts'
+    __searchable__ = ['body']
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    cart_items = db.relationship(
+        'Tag',
+        secondary=tags,
+        backref='author',
+    )
