a743901a41089651faf77162efa28df8ca850ae8
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-04-10 17:28:47 -0400

    add wait-for-it

--- stationaryshop/entrypoint.sh
+++ stationaryshop/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+echo waiting for db
+./wait-for-it.sh db:5432 -t 5
+
 flask db upgrade
 flask seed-db
+
+echo waiting for redis
+./wait-for-it.sh redis:6379 -t 5
+
+echo executing gunicorn
 gunicorn -b :5000 --access-logfile - --error-logfile - stationaryshop:app --timeout 120
